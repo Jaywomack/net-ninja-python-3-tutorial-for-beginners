b2f64e38e692dc1f315209d05e4dda2e1417b6ef
jaywomack <womackjw@hotmail.com>
2020-09-15 18:05:44 -0700

    Finished if statements

---
+++ if_statements.py
@@ -0,0 +1,19 @@
+# OPERATORS
+# <,>,<=,>=,!,!=, ==,
+# # age = int(input("Enter your age:"))
+
+# if age < 10:
+#     # code block
+#     print("you're young, strange one")
+# elif age < 40:
+#     # do something
+#     print('the fire in you is strong, strange one')
+# else:
+#     print("You are wise beyond doubt, strange one")
+
+meaty = input("Do you eat meat? (y/n):")
+
+if meaty == 'y':
+    print("Eat the Meat!")
+elif meaty == 'n':
+    print("Vegetables for you!")
