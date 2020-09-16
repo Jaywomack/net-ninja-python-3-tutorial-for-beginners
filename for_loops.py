ca007290b59d819fd28f11194cec1dc299380c44
jaywomack <womackjw@hotmail.com>
2020-09-15 19:29:26 -0700

    end day

---
+++ for_loops.py
@@ -0,0 +1,23 @@
+# ninjas = ['ryu', 'crystal', 'yoshi', 'ken']
+
+# # for ninja in ninjas:
+# #     print(ninja)
+
+# # for ninja in ninjas:
+# #     print(ninjas)
+
+# for ninja in ninjas:
+#     if ninja == 'yoshi':
+#         print(f'{ninja} - black belt')
+#         break
+#     else:
+#         print(ninja)
+
+# WHILE LOOP
+age = 25
+num = 0
+
+while num <= age:
+    if num % 2 == 0:
+        print(num)
+    num += 1
