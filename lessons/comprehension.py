3289d14bf26859c14cff39c3f9b0ee2dd9eb5c53
Jay Womack <womackjw@hotmail.com>
2020-09-27 20:54:03 -0700

    working on comprehesions

---
+++ lessons/comprehension.py
@@ -0,0 +1,11 @@
+prizes = [5, 10, 50, 100, 1000]
+
+dbl_prizes = []
+for prize in prizes:
+    dbl_prizes.append(prize*2)
+
+print(dbl_prizes)
+
+
+# comprehesion method
+dbl_prizes = [prize*2 for prize in prizes]
